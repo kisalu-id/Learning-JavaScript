209e2eba82cd1ee62e36f99f525876a0ba24412f
kisalu-id <152627403+kisalu-id@users.noreply.github.com>
2024-03-03 00:01:53 +0100

    Create Pop-up boxes.js

---
+++ Basics/Pop-up boxes.js
@@ -0,0 +1,11 @@
+// Make your prompt box below!
+var test1 = prompt("Type Hi!");
+console.log(test1);
+
+var test2 = confirm("You said " + test1);
+console.log(test2);
+
+var test3 = alert("Bye!");
+console.log(test3);
+
+/*Confirm boxes will return "true" if ok is selected, and return "false" if cancel is selected. Alert boxes will not return anything. Prompt boxes will return whatever is in the text box. Note: prompt boxes also have an optional second parameter, which is the text that will already be in the text box.*/
