052464f62401f313c794737e56d2f9a864ed1c43
kisalu-id <152627403+kisalu-id@users.noreply.github.com>
2024-03-02 23:54:41 +0100

    Create Anonymous function.js

---
+++ Basics/Anonymous function.js
@@ -0,0 +1,9 @@
+var multiplyFive = function(num) //instead of "function multiplyFive(num)", in js functions work like objects
+{
+    return (num * 5);
+}
+
+console.log(multiplyFive(3));
+console.log(multiplyFive(4));
+console.log(multiplyFive(5));
+console.log(multiplyFive(6));
