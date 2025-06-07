88082a5f134aa1c5287f411399e55310a8dd3aaa
Diana <di.ulasik@gmail.com>
2025-06-07 14:00:00 +0200

    Create Quotes.js

---
+++ Basics/Quotes.js
@@ -0,0 +1,24 @@
+for (let i = 3; i > 0; i--) {
+    console.log("Hello World! " + i);
+}
+
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`7 x ${i} = ${7 * i}`);
+}
+
+let message = `Hello
+This is on a new line.`;
+console.log(message);
+/*
+single quotes ' ' and double quotes " " are interchangeable - they behave the same way
+let name1 = 'Alice';
+let name2 = "Bob";
+
+to use variables inside strings, use template literals ` ` (backticks)
+${expression}
+
+multiline strings:
+let message = `Hello
+This is on a new line.`;
+*/
